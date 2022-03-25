60e00f43972df1c1a3e7d1f45826eb737b31456f
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-25 15:37:41 +0000

    Ponemos ejemplos de correos para ver si funciona

--- CLASES/Ejercicio1.py
+++ CLASES/Ejercicio1.py
@@ -10,7 +10,6 @@ import re
 
 
 #Empezamos con el código principal
-mail_server = smtplib.SMTP("localhost")
 
 while True:
     try:
@@ -32,3 +31,13 @@ while True:
 def correo_valido(correo):
     expresion_regular = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
     return re.match(expresion_regular, correo) is not None
+
+# Ejemplos de correos
+correos = [
+    "hola@", "ruben@profe.me", "soypaula@gmail.com",
+    "ejercicio@excepciones.com", "correo@hotmail.com", "poo@gmail.com", "fallo",
+    "no@se@hacerlo.com"
+]
+
+for correo in correos:
+    print("Vamos a comprobar si el correo introducido '{}' es válido...{}".format(correo, correo_valido(correo)))
